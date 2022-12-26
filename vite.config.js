e62fe91cd84942279a3536e2ce5fb5c31c045ed3
vinicius <vinicius.dogonski@gmail.com>
2022-12-26 15:19:10 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/portifolio/'
+    base: '/portifolio/',
+    build: {
+        chunkSizeWarningLimit: 1600,
+    },
 })
